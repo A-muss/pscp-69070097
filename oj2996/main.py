0f369537ad1d6edfdf48cd6a35d3249254db0219
Amus <69070097@kmitl.ac.th>
2026-07-07 21:31:19 +0700

    week 1 submission

--- oj2996/main.py
+++
@@ -1,7 +0,0 @@
-"""docstring module"""
-def main():
-    """main"""
-    text = input().lower()
-    print(text[::-1])
-
-main()
--- oj3016/main.py
+++
@@ -1,9 +0,0 @@
-"""seven"""
-def main():
-    """main"""
-    x = int(input())
-    result = x % 4
-    pattern = [1, 7, 9, 3]
-    print(pattern[result])
-
-main()
